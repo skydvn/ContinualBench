db2d475454a57795fca9bfb02616264152e086ab
daothanh2011 <daothanhbka@gmail.com>
2025-10-21 14:34:26 +0900

    add the autoscript files for csrel and run_pbcs methods

---
+++ run_pbcs.sh
@@ -0,0 +1,18 @@
+python3.11 main.py --model pbcs --dataset seq-cifar10 --buffer_size 200 --pbcs_epoch_converge 50 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type sample
+
+python3.11 main.py --model pbcs --dataset seq-cifar10 --buffer_size 200 --pbcs_epoch_converge 50 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type pixel
+
+
+python3.11 main.py --model pbcs --dataset seq-cifar10 --buffer_size 400 --pbcs_epoch_converge 150 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type sample
+
+python3.11 main.py --model pbcs --dataset seq-cifar10 --buffer_size 400 --pbcs_epoch_converge 150 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type pixel
+
+
+python3.11 main.py --model pbcs --dataset seq-cifar100 --buffer_size 200 --pbcs_epoch_converge 50 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type sample
+
+python3.11 main.py --model pbcs --dataset seq-cifar100 --buffer_size 200 --pbcs_epoch_converge 50 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type pixel
+
+
+python3.11 main.py --model pbcs --dataset seq-cifar100 --buffer_size 400 --pbcs_epoch_converge 150 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type sample
+
+python3.11 main.py --model pbcs --dataset seq-cifar100 --buffer_size 400 --pbcs_epoch_converge 150 --lr 0.001 --batch_size 256 --n_epochs 400 --pbcs_selection_type pixel
