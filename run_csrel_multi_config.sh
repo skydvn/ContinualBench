6fbd20860e5c6266b01f4f50bb06838e59e5abd7
daothanh2011 <daothanhbka@gmail.com>
2025-10-21 14:36:11 +0900

    add the autoscript files for csrel and run_pbcs methods

--- run_csrel_multi_config.sh
+++ run_csrel_multi_config.sh
@@ -2,6 +2,10 @@ bash run_csrel_config.sh seq-cifar10 200 0.001 150 0.003 cuda:0
 
 bash run_csrel_config.sh seq-cifar100 200 0.002 10 0.003 cuda:0
 
+bash run_csrel_config.sh seq-cifar10 400 0.001 150 0.003 cuda:0
+
+bash run_csrel_config.sh seq-cifar100 400 0.002 10 0.003 cuda:0
+
 #bash run_csrel_config.sh mnist-360 100 0.0005 20 0.003 cuda:0
 
 #bash run_csrel_config.sh perm-mnist 200 0.001 150 0.003 cuda:0
